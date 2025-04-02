5f613c72d477b7548340b478fced4537baa03113
janozeli <lucasmjanozeli@gmail.com>
2025-04-02 16:48:04 -0300

    working asteroid field

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def main():
 
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
-    AsteroidField.containers = (updatable,)
+    AsteroidField.containers = updatable
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroid_field = AsteroidField()
